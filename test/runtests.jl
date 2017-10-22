c67e12b2efb0b22634e7c1f334e0a68d33ac4a7a
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 10:01:07 +0200

    Use Compat.Test instead of Base.Test for Julia v0.7 compatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of H3DPolaris.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package H3DPolaris" begin
+import Compat.Test
+Test.@testset "Package H3DPolaris" begin
     include.([
     ])
 end
